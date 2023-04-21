e3e346b383cd4c5e7cd3b41b49e47599a5640183
NahuelTapia <111470674+NahuelTapia@users.noreply.github.com>
2023-04-20 22:15:19 -0300

    Actualizacion puntos 1.4 ; 1.5 ; 1.6 ;

--- Python/manejo_exepcione.py
+++ Python/manejo_exepcione.py
@@ -1,7 +1,12 @@
+from NumerosIgualesException import NumerosIgualesException
+
 resultado = None
-a = 7
-b = 0
+
 try:
+    a = int(input("Digite el primer numero: "))
+    b = int(input("Digite el segundo numero: "))
+    if a == b:
+        raise NumerosIgualesException("Son numeros iguales")
     resultado = a / b #modificamos
 except TypeError as e:
     print(f'Type Error - Ocurrio un error {type(e)}')
@@ -9,6 +14,10 @@ except ZeroDivisionError as  e:
     print(f'ZerDivisionError - Ecurrio un error {e}')
 except Exception as e:
     print(f'Execption - Ocurrio un error: {type(e)}')
+else:
+    print("No se arrojo ninguna excepcion")
+finally: #siempre se va a ejecutar
+    print("Ejecucion de este bloque finally")
 
 print(f'El resultado es: {resultado}')
 print('seguimos...')
