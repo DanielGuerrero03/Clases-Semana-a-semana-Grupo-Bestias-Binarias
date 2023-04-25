55d319711358ab3ab1b1c9314c53f9b9fd42d5ff
Florencia Ortega <florortega055@gmail.com>
2023-04-24 21:24:35 -0300

    Añadimos puntos 1.4, 1.5, 1.6 de clase 2 Python

---
+++ Python/clase2-item1.5.py
@@ -0,0 +1,12 @@
+from ManejoArchivos import ManejoArchivos
+
+# MANEJO DE CONTEXTO WITH: sintaxis simplificada, abre y cierra el archivo
+with open('prueba.txt','r',encoding='utf8') as archivo:
+    print(archivo.read())
+# No hace falta ni el try, ni el finally
+# en el contexto de with lo que se ejecuta de manera automatica
+# Utiliza diferentes metodos: __enter__
+# Ahora el siguiente metodo es el que cierra: __exit__
+
+with ManejoArchivos('prueba.txt') as archivo:
+    print(archivo.read())
