fc059b2ec741d967aa7d11bc27de44a93e00b856
DaianaEscudero <112595487+DaianaEscudero@users.noreply.github.com>
2023-04-27 00:13:53 -0300

    Agrego lo pedido en issue
    Codigo JavaScript CLASE 3. 1.1 Introducción a funciones - 1.2 Palabra return

---
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -0,0 +1,12 @@
+miFunción(8, 2); //Esto se lo conoce como hosting
+
+function miFunción(a,b){
+    //console.log("Sumamos: "+ (a + b));
+    return a + b;
+}
+
+//Llamando la función
+miFunción(5, 4);
+
+let resultado = miFunción(6, 7);
+console.log(resultado);
