393cea23749ecf080c7c91e657f04360e4e2f000
NahuelTapia <111470674+NahuelTapia@users.noreply.github.com>
2023-05-03 18:25:31 -0300

    Actividad 1.7 realizada
    Añadimos distintas formas de imprimir un Objeto como: Object.values()  y JSON.stringify()

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -31,3 +31,25 @@ console.log('cambiamos y eliminamos un error');
 persona.apellida = 'Betancud';//Cambiamos dinamicamnete un valor del objeto
 delete persona.apellido= 'Betancud'; //Eliminamos el error
 console.log(persona);
+
+
+//Distintas formas de imprimir un objeto
+//Numero 1: la mas sencilla: concatenar cada valor de cada propiedad
+console.log("Distintas formas de imprimir un objeto: forma 1");
+console.log(persona.nombre+", "+persona.apellido);
+
+//Numero 2: a traves del ciclo for in
+console.log("Distintas formas de imprimir un objeto: forma 2");
+for(nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+//Numero 3: La funcion Object.values()
+console.log("Distintas formas de imprimir un objeto: forma 3");
+let personaArray = Object.values(persona);
+console.log(personaArray);
+
+//Numero 4: utilizaremos el metodo JSON.stringify
+console.log("Distintas formas de imprimir un objeto: forma 4");
+let personaString = JSON.stringify(persona);
+console.log(personaString);
