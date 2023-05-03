2824db59d3b01898b29ea13c0e4a988538d3cd17
FernandoSilva12 <112596349+FernandoSilva12@users.noreply.github.com>
2023-05-03 17:37:16 -0300

    Clase 1.5 y 1.6
    Mis comits están terminados.

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -1,6 +1,6 @@
 let x = 10; //variable de tipo primitiva
 console.log(x.length); 
-
+console.log('Tipos Primitivos')
 //Objeto
 let persona = {
     nombre: 'Carlos',
@@ -19,3 +19,15 @@ console.log(persona.email);
 console.log(persona.edad);
 console.log(persona);
 console.log(persona.nombreCompleto());
+console.log('Ejecutando con un objeto');
+
+
+console.log('Creamos un nuevo objeto');
+
+
+console.log('usamos el ciclo for in')
+////
+console.log('cambiamos y eliminamos un error');
+persona.apellida = 'Betancud';//Cambiamos dinamicamnete un valor del objeto
+delete persona.apellido= 'Betancud'; //Eliminamos el error
+console.log(persona);
