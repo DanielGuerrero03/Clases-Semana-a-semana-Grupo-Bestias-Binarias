4928ebbd34153633e0c1650c465ad68247459fb9
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-05-17 18:21:21 -0300

    Agrego punto 1.9 y 1.10 delete_registro y delete_varios_reg

---
+++ Python/Leccion04/delete_registro.py
@@ -0,0 +1,23 @@
+import psycopg2  # Esto es para poder conectarnos a Postgres
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='postgres',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona=%s'
+            entrada = input('Digite el número de registro a eliminar: ')
+            valores = (entrada,)
+            cursor.execute(sentencia, valores) # De esta manera ejecutamos la sentencia
+            registros_eliminados = cursor.rowcount
+            print(f'Los registros eliminados son : {registros_eliminados}')
+
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    conexion.close()
