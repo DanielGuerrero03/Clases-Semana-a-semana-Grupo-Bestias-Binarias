f6bcff3ab62ac79dc569db70085702eafe11d83b
NahuelTapia <111470674+NahuelTapia@users.noreply.github.com>
2023-06-01 17:36:35 -0300

    Agrego: log.basicConfig

--- Python/Leccion07/logger_base.py
+++ Python/Leccion07/logger_base.py
@@ -3,6 +3,14 @@ import logging as log
 # docs.python.org/3/howto/logging.html
 # Llamamos una configuracion basica
 
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])
+
 if __name__ == '__main__':
 
     log.debug('Mensaje a nivel debug')
