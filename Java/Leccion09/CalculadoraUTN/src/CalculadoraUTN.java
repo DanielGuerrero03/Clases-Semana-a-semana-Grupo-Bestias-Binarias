4489903862a9cc61d2365c9ff10875924ad1e275
Carlos David Mato <104330698+MCDavid1972@users.noreply.github.com>
2023-06-14 21:42:51 -0300

    Update CalculadoraUTN.java

--- Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -8,7 +8,6 @@ public class CalculadoraUTN {
         var operando1 = Integer.parseInt(entrada.nextLine());
         System.out.print("Ingrese el valor para el operando2: ");
         var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
+      
     }
 }
