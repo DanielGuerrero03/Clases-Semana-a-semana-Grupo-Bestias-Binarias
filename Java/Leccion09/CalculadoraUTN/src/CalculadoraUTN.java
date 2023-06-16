369635d016d0a025185e90a928b3c46dec3b4d2b
Albanoo09 <105444630+Albanoo09@users.noreply.github.com>
2023-06-16 12:05:50 -0300

    Add files via upload
    Agrego videos 8 y 9 de semana 9 Java

--- Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -4,23 +4,46 @@ public class CalculadoraUTN {
         Scanner entrada = new Scanner(System.in);
          while (true) { //Ciclo infinito
         System.out.println("********** Aplicacion Calculadora **********");
-        //Mostrar el Menú
-        System.out.print("""
-                         |1. Sumar.
-                         |2. Resta
-                         |3. Multiplicación
-                         |4. División
-                         |5. Salir
-                         """);
-        System.out.println("Operacion a realizar?");     
+        mostrarMenu();
      try {
         var operacion = Integer.parseInt(entrada.nextLine());
-        If(operacion >= 1 && operacion <= 4 ) {
-            System.out.print("Ingrese el valor para el operando1: ");
-            var operando1 = Integer.parseInt(entrada.nextLine());
+        if (operacion >= 1 && operacion <= 4 ) {
+            
+         ejecutarOperacion(operacion, entrada);
+            } //Fin del if
+            else if (operacion == 5) {
+                System.out.println("Hasta pronto...");
+                break; //Rompe el ciclo y sale
+            } else {
+                System.out.println("Opcion erronea: " + operacion);
+            }
+            //Imprimimos un salto de linea antes de repetir el menu
+            System.out.println();
+        } catch (Exception e){ // Fin try, comienzo del catch
+            System.out.println("Ocurrio un error: " +e.getMessage());
+            System.out.println();
+
+        }
+        } //Fin while
+    } //Fin main
+    
+    private static void mostrarMenu(){
+        //Mostrar el Menu
+        System.out.println("""
+                         |1. Sumar.
+                         |2. Resta
+                         |3. Multiplicacion
+                         |4. Division
+                         |5. Salir
+                         """);
+        System.out.println("Operacion a realizar?");
+    }// Fin metodod mostrar meno
+    private static void ejecutarOperacion(int operacion, Scanner entrada){
+        System.out.print("Ingrese el valor para el operando1: ");
+            var operando1 = Double.parseDouble(entrada.nextLine());
             System.out.print("Ingrese el valor para el operando2: ");
-            var operando2 = Integer.parseInt(entrada.nextLine());
-         int resultado;
+            var operando2 = Double.parseDouble(entrada.nextLine());
+        Double resultado;
                 switch (operacion) {
                     case 1 -> { //Suma
                         resultado = operando1 + operando2;
@@ -41,20 +64,5 @@ public class CalculadoraUTN {
                     default -> System.out.println("Opcion erronea:" + operacion);
 
                 } //Fin switch
-            } //Fin del if
-            else if (operacion == 5) {
-                System.out.println("Hasta pronto...");
-                break; //Rompe el ciclo y sale
-            } else {
-                System.out.println("Opcion erronea: " + operacion);
-            }
-            //Imprimimos un salto de linea antes de repetir el menu
-            System.out.println();
-        } catch (Exception e){ // Fin try, comienzo del catch
-            System.out.println("Ocurrio un error: " +e.getMessage());
-            System.out.println();
-
-        }
-        } //Fin while
-    } //Fin main
+    }//fin metodo ejecutar operacion 
 } //Fin clase
