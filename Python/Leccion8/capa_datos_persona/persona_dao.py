c6802c2bcf16208dfe83b1569665530a88401979
Florencia Ortega <florortega055@gmail.com>
2023-06-16 21:56:22 -0300

    Añado semana 9 clase 9 Videos 1-2 PYTHON

--- Python/Leccion8/capa_datos_persona/persona_dao.py
+++ Python/Leccion8/capa_datos_persona/persona_dao.py
@@ -17,20 +17,21 @@ class PersonaDAO:
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
 
+    #Definimos los metodos de clases
 
-
-
-
-
-
-
-
-
-
-
-
+    @classmethod()
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexoin.obtenerConexion() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[3])
+                    personas.append(persona)
+                    return personas
+            
     
-
     @classmethod
     def eliminar(cls, persona):
         with Conexion.obtenerConexion():
@@ -42,10 +43,10 @@ class PersonaDAO:
 
 
 if __name__ == '__main__':
-
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
     #Eliminar un registro
     #persona1 = Persona(id_persona=13)
     #personas_eliminadas = PersonaDAO.eliminar(persona1)
     #log.debug(f'Personas eliminadas: {personas_eliminadas}')
-
- 
