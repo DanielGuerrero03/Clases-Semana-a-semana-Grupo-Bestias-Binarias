5afe5a76ab04dd9224b5687350db5c6e9cfaf619
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-06-30 17:59:35 -0300

    Agrego clase 12 java Parate 1,2 y 3

--- Java/Leccion12/SistemaEstudiante/src/main/java/UTN/Main.java
+++ Java/Leccion12/SistemaEstudiante/src/main/java/UTN/Main.java
@@ -4,16 +4,11 @@ package UTN;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var conexion = UTN.conexion.Conexion.getConnection();
+        if (conexion != null)
+            System.out.println("Conexion existosa: "+conexion);
+         else
+            System.out.println("Error al conectar con la base de datos");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
-}
+    } // Fin metodo main
+} // Fin clase Main
